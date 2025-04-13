96d0c0a557f76d99e307c90ca3f14e2162c7c2da
Carlos Victor <carlosvictortecprof@gmail.com>
2025-04-13 16:31:24 -0300

    feat: adicionando codigo do bot

---
+++ main.py
@@ -0,0 +1,48 @@
+from discord import Intents, utils, AllowedMentions
+from discord.ext import commands
+from decouple import config
+
+token = config("DISCORD_TOKEN", cast=str)
+
+intents = Intents.default()
+intents.members = True
+intents.presences = True
+intents.guilds = True
+intents.voice_states = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot conectado como {bot.user}")
+
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if member.bot:
+        return
+
+    if all([bool(before.channel), bool(after.channel)]):
+        return
+
+    guild = member.guild
+    members_channels = [
+        member for channel in guild.voice_channels for member in channel.members
+    ]
+    actual_members = [
+        member_channel for member_channel in members_channels if not member_channel.bot
+    ]
+
+    if len(actual_members) != 1:
+        return
+
+    general_channel = utils.get(guild.text_channels, name="geral")
+    if general_channel:
+        await general_channel.send(
+            f"{member.mention} Chegou e está Chamando Todos os Autobots\n@everyone",
+            allowed_mentions=AllowedMentions(everyone=True),
+        )
+
+
+bot.run(token)
